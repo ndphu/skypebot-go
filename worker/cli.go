df1a5e18cb40da821c8c3acf2ba9f22c115619dc
phudnguyen <phudnguyen@kms-technology.com>
2020-08-13 01:06:11 +0700

    update

--- worker/cli.go
+++ worker/cli.go
@@ -3,6 +3,7 @@ package worker
 import (
 	"bytes"
 	"fmt"
+	"github.com/ndphu/skypebot-go/media"
 	"github.com/ndphu/skypebot-go/model"
 	"github.com/ndphu/skypebot-go/utils"
 	"github.com/olekukonko/tablewriter"
@@ -48,7 +49,7 @@ func (w *Worker) ConversationCommand() *cli.Command {
 
 func (w *Worker) CovidCommand() *cli.Command {
 	return &cli.Command{
-		Name:    "covid19",
+		Name: "covid19",
 		Subcommands: []*cli.Command{
 			{
 				Name:    "update",
@@ -73,33 +74,78 @@ func (w *Worker) CovidCommand() *cli.Command {
 	}
 }
 
-func (w *Worker) NewAdminCLI() *cli.App {
+func (w *Worker) NsfwCommand(threadId string) *cli.Command {
+	var commands []*cli.Command
+
+	commands = append(commands, &cli.Command{
+		Name: "keyword",
+		Action: func(c *cli.Context) error {
+			fmt.Fprintln(c.App.Writer, "Available keyword:")
+			fmt.Fprintln(c.App.Writer, strings.Join(media.GetKeywords(), ", "))
+			return nil
+		},
+	})
+	for _, kw := range media.GetKeywords() {
+		x := kw
+		commands = append(commands,&cli.Command{
+			Name: x,
+			Action: func(c *cli.Context) error {
+				utils.ExecuteWithRetry(func() error {
+					return w.sendRandomImage(threadId, x)
+				})
+				return nil
+			},
+		})
+	}
+
+	return &cli.Command{
+		Name: "nsfw",
+		Flags: []cli.Flag{
+
+		},
+		Subcommands: commands,
+	}
+}
+
+func (w *Worker) NewAdminCLI(threadId string) *cli.App {
 	return &cli.App{
 		Name:      "admin-cli",
 		UsageText: "skype-bot admin cli",
 
 		Commands: []*cli.Command{
 			w.ConversationCommand(),
+			w.NsfwCommand(threadId),
 		},
 
 		ExitErrHandler: func(c *cli.Context, err error) {
 		},
 		CommandNotFound: func(c *cli.Context, cmd string) {
-			fmt.Fprintf(c.App.Writer, "Command not found: [%s]. See help message for details.\n", cmd)
+			if c.Command.Name == "nsfw" {
+				//TODO hack
+				utils.ExecuteWithRetry(func() error {
+					return w.sendRandomImage(threadId, cmd)
+				})
+			} else {
+				fmt.Fprintf(c.App.Writer, "Command not found: [%s]. See help message for details.\n", cmd)
+			}
 		},
 	}
 }
 
 func (w *Worker) HandleAdminCommand(event *model.MessageEvent) error {
-	adminCLI := w.NewAdminCLI()
+	adminCLI := w.NewAdminCLI(event.GetThreadId())
 	var buff bytes.Buffer
 	adminCLI.Writer = &buff
 	adminCLI.ErrWriter = &buff
 	content := normalizeMessageContent("admin-cli " + event.Resource.Content)
 	if err := adminCLI.Run(strings.Split(content, " ")); err != nil {
+
 		return w.SendTextMessage(event.GetThreadId(), "<pre>"+buff.String()+"</pre>")
 	}
 	return utils.ExecuteWithRetry(func() error {
+		if buff.Len() == 0 {
+			return nil
+		}
 		return w.SendTextMessage(event.GetThreadId(), "<pre>"+buff.String()+"</pre>")
 	})
 }
